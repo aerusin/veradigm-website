a2df48f235e29619858e6267f89bec48fe9ea819
Alexander Rusin <arusin@udel.edu>
2023-03-27 17:26:36 -0400

    created website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import doctors from "./doctors";
 
 function App() {
+  const [selectedDoctor, setSelectedDoctor] = useState(null);
+
+  const handleClick = (index) => {
+    setSelectedDoctor(doctors[index]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>List of Doctors</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Specialty</th>
+            <th>Location</th>
+            <th>Review Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          {doctors.map((doctor, index) => (
+            <tr key={index} onClick={() => handleClick(index)}>
+              <td>{doctor.name}</td>
+              <td>{doctor.specialty}</td>
+              <td>{doctor.location}</td>
+              <td>{doctor.reviewScore}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {selectedDoctor && (
+        <div>
+          <h2>Doctor Details</h2>
+          <p>{selectedDoctor.details}</p>
+          <h2>Similar Doctors:</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Specialty</th>
+                <th>Location</th>
+                <th>Review Score</th>
+              </tr>
+            </thead>
+            <tbody>
+              {doctors
+                //filter by same specialty and location
+                .filter(
+                  (doctor) =>
+                    doctor.specialty === selectedDoctor.specialty &&
+                    doctor.location === selectedDoctor.location &&
+                    doctor.name !== selectedDoctor.name
+                )
+                // sort with the highest review score at the top
+                .sort((a, b) => b.reviewScore - a.reviewScore)
+                .map((doctor, index) => (
+                  <tr key={index}>
+                    <td>{doctor.name}</td>
+                    <td>{doctor.specialty}</td>
+                    <td>{doctor.location}</td>
+                    <td>{doctor.reviewScore}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/doctors.js
@@ -0,0 +1,116 @@
+const doctors = [
+  {
+    name: "Dr. Emily Lee",
+    specialty: "Dermatology",
+    location: "Lost Angeles",
+    reviewScore: 4.8,
+    details:
+      "Dr. Emily Lee is a dermatologist with expertise in cosmetic dermatology.",
+  },
+  {
+    name: "Dr. Michael Johnson",
+    specialty: "Oncology",
+    location: "Los Angeles",
+    reviewScore: 4.5,
+    details:
+      "Dr. Michael Johnson is an oncologist with over 15 years of experience in treating cancer patients.",
+  },
+  {
+    name: "Dr. Sarah Brown",
+    specialty: "Pediatrics",
+    location: "Chicago",
+    reviewScore: 4.7,
+    details:
+      "Dr. Sarah Brown is a pediatrician with expertise in child nutrition and development.",
+  },
+  {
+    name: "Dr. David Kim",
+    specialty: "Cardiology",
+    location: "New York",
+    reviewScore: 4.6,
+    details:
+      "Dr. David Kim is a cardiologist with a focus on heart disease prevention.",
+  },
+  {
+    name: "Dr. Rachel Chen",
+    specialty: "Neurology",
+    location: "San Francisco",
+    reviewScore: 4.9,
+    details:
+      "Dr. Rachel Chen is a neurologist with expertise in treating neurological disorders.",
+  },
+  {
+    name: "Dr. Jonathan Lee",
+    specialty: "Orthopedics",
+    location: "Los Angeles",
+    reviewScore: 4.4,
+    details:
+      "Dr. Jonathan Lee is an orthopedic surgeon with a focus on sports medicine.",
+  },
+  {
+    name: "Dr. John Smith",
+    specialty: "Cardiology",
+    location: "New York",
+    reviewScore: 4.5,
+    details:
+      "Dr. John Smith is a renowned cardiologist with over 20 years of experience. He specializes in treating complex heart conditions and has a track record of successful surgeries and treatments. He is known for his compassionate care and attention to detail.",
+  },
+  {
+    name: "Dr. Sarah Lee",
+    specialty: "Dermatology",
+    location: "Los Angeles",
+    reviewScore: 4.8,
+    details:
+      "Dr. Sarah Lee is a board-certified dermatologist with a focus on cosmetic dermatology. She is a skilled injector and has a keen eye for detail. She is known for her natural-looking results and personalized approach to each patient.",
+  },
+  {
+    name: "Dr. Michael Chen",
+    specialty: "Orthopedics",
+    location: "New York",
+    reviewScore: 4.3,
+    details:
+      "Dr. Michael Chen is a board-certified orthopedic surgeon with a subspecialty in sports medicine. He is known for his minimally invasive surgical techniques and successful outcomes. He is also a team physician for several professional sports teams in the area.",
+  },
+  {
+    name: "Dr. Emily Kim",
+    specialty: "Pediatrics",
+    location: "Los Angeles",
+    reviewScore: 4.7,
+    details:
+      "Dr. Emily Kim is a pediatrician with over 15 years of experience. She is known for her gentle and caring approach to children and their families. She also has a special interest in childhood development and behavior.",
+  },
+  {
+    name: "Dr. David Lee",
+    specialty: "Cardiology",
+    location: "New York",
+    reviewScore: 4.4,
+    details:
+      "Dr. David Lee is a board-certified cardiologist with a focus on interventional cardiology. He is skilled in performing complex procedures such as angioplasty and stenting. He is known for his excellent bedside manner and dedication to his patients.",
+  },
+  {
+    name: "Dr. Lisa Kim",
+    specialty: "Dermatology",
+    location: "Los Angeles",
+    reviewScore: 4.9,
+    details:
+      "Dr. Lisa Kim is a board-certified dermatologist with a focus on medical dermatology. She has a special interest in treating patients with skin cancer and has extensive experience in Mohs surgery. She is known for her compassionate care and attention to detail.",
+  },
+  {
+    name: "Dr. James Lee",
+    specialty: "Orthopedics",
+    location: "New York",
+    reviewScore: 4.2,
+    details:
+      "Dr. James Lee is a board-certified orthopedic surgeon with a subspecialty in hip and knee replacements. He is known for his minimally invasive techniques and dedication to his patients' recovery. He is also a frequent presenter at orthopedic conferences and seminars.",
+  },
+  {
+    name: "Dr. Brian Park",
+    specialty: "Pediatrics",
+    location: "Los Angeles",
+    reviewScore: 4.6,
+    details:
+      "Dr. Brian Park is a pediatrician with over 10 years of experience. He has a special interest in asthma and allergies and has developed a unique treatment approach that has helped many of his patients. He is known for his friendly demeanor and thorough explanations of medical conditions.",
+  },
+];
+
+export default doctors;
